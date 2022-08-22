485783a5ee7eb3e74ce0e7c11d2f6eea9fb5f533
VasiliiOptimist <vasya_mk@mail.ru>
2022-08-22 23:03:29 +0300

    Add login

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '607713711a66ac0fbb888629'
 db = SQLAlchemy(app)  # writing 'app' is important
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
+from market import bcrypt
 
 
 class RegisterForm(FlaskForm):
@@ -20,3 +21,9 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
